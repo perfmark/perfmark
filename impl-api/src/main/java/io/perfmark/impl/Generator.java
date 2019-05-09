a2ce713cdb2ad9442a0b6f62b81280e80a9a85b3
Carl Mastrangelo <carl@carlmastrangelo.com>
2019-05-09 02:34:56 -0700

    add Generator docs

--- impl-api/src/main/java/io/perfmark/impl/Generator.java
+++ impl-api/src/main/java/io/perfmark/impl/Generator.java
@@ -1,12 +1,45 @@
 package io.perfmark.impl;
 
+import javax.annotation.concurrent.NotThreadSafe;
+
+/**
+ * A Generator keeps track of what generation the PerfMark library is on.  A generation is a way to
+ * group PerfMark recorded tasks and links together.  This allows dynamically enabling and
+ * disabling PerfMark.   Each time the library is enabled, a new generation is started and
+ * associated with all the recorded data.
+ *
+ * <p>Normal users are not expected to use this class.
+ */
+@NotThreadSafe
 public abstract class Generator {
+  /**
+   * The number of reserved bits at the bottom of the generation.   All generations should be
+   * left-shifted by this amount.
+   */
   public static final int GEN_OFFSET = 8;
+  /**
+   * Represents a failure to enable PerfMark library.  This can be used by subclasses to indicate
+   * a previous failure to set the generation.  It can also be used to indicate the generation count
+   * has overflowed.
+   */
   public static final long FAILURE = -2L << GEN_OFFSET;
 
   protected Generator() {}
 
+  /**
+   * Sets the current generation count.  This should be eventually noticeable for callers of
+   * {@link #getGeneration()}.  An odd number means the library is enabled, while an even number
+   * means the library is disabled.
+   *
+   * @param generation the generation id, shifted left by {@link #GEN_OFFSET}.
+   */
   public abstract void setGeneration(long generation);
 
+  /**
+   * Gets the current generation, shifted left by {@link #GEN_OFFSET}.  An odd number means the
+   * library is enabled, while an even number means the library is disabled.
+   *
+   * @return the current generation or {@link #FAILURE}.
+   */
   public abstract long getGeneration();
 }
