6c2ad315ca99408ede8a027f137f6a047e5e386e
Carl Mastrangelo <carl@carlmastrangelo.com>
2019-05-09 02:22:21 -0700

    add Link docs

--- api/src/main/java/io/perfmark/Link.java
+++ api/src/main/java/io/perfmark/Link.java
@@ -4,7 +4,13 @@ import io.perfmark.impl.Mark;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * A link represents a linkage between asynchronous tasks.
+ * A link represents a linkage between asynchronous tasks.  A link is created inside of a started
+ * task.  The resulting {@link Link} object can then be passed to other asynchronous tasks to
+ * associate them with the original task.
+ *
+ * <p>A source task may have multiple outbound links pointing to other tasks.  However, calling
+ * {@link Link#link()} only work if it is the first such call.  Subsequent calls to {@code link()}
+ * have no effect.
  */
 public final class Link {
   static final Link NONE = new Link(Mark.NO_LINK_ID);
@@ -17,6 +23,10 @@ public final class Link {
     this.id = linkId;
   }
 
+  /**
+   * Associate this link with the most recently started task.  There may be at most one inbound
+   * linkage per task: the first call to {@code link()} decides which outbound task is the origin.
+   */
   public void link() {
     PerfMark.link(id);
   }
