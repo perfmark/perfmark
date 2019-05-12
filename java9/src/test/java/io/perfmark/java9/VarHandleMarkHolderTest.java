58cd2fc684f10b51532a5497beb3735a2d3b5471
Carl Mastrangelo <carl@carlmastrangelo.com>
2019-05-12 10:08:53 -0700

    rename PerfMarkStorageTest

--- java9/src/test/java/io/perfmark/java9/VarHandleMarkHolderTest.java
+++ java9/src/test/java/io/perfmark/java9/VarHandleMarkHolderTest.java
@@ -23,7 +23,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class PerfMarkStorageTest {
+public class VarHandleMarkHolderTest {
 
   private final long gen = 1L << Generator.GEN_OFFSET;
 
