c938fc0bb632d6b8de8a8ae254e41d88efee6451
Carl Mastrangelo <carl@carlmastrangelo.com>
2019-05-16 14:33:46 -0700

    Add a Java6 Markholder benchmark.
    On Skylake, performance governer, 11.0.2:
    
    perfmark-java6
    Benchmark                                                 Mode  Cnt   Score   Error  Units
    VolatileGeneratorBenchmark.getAndSetAndGetGeneration      avgt    5   7.777 ± 0.098  ns/op
    VolatileGeneratorBenchmark.getGeneration                  avgt    5   2.183 ± 0.027  ns/op
    VolatileGeneratorBenchmark.racyGetAndSetAndGetGeneration  avgt    5  89.734 ± 5.932  ns/op
    SynchronizedMarkHolderBenchmark.link                      avgt    5   5.195 ± 0.065  ns/op
    SynchronizedMarkHolderBenchmark.start_marker_noTag        avgt    5   4.663 ± 0.036  ns/op
    SynchronizedMarkHolderBenchmark.start_marker_tag          avgt    5   6.326 ± 0.271  ns/op
    SynchronizedMarkHolderBenchmark.start_name_noTag          avgt    5   5.246 ± 0.025  ns/op
    SynchronizedMarkHolderBenchmark.start_name_tag            avgt    5   6.176 ± 0.056  ns/op
    SynchronizedMarkHolderBenchmark.stop_marker_noTag         avgt    5   4.602 ± 0.083  ns/op
    SynchronizedMarkHolderBenchmark.stop_marker_tag           avgt    5   6.458 ± 0.169  ns/op
    SynchronizedMarkHolderBenchmark.stop_name_noTag           avgt    5   5.186 ± 0.124  ns/op
    SynchronizedMarkHolderBenchmark.stop_name_tag             avgt    5   6.008 ± 0.080  ns/op
    
    perfmark-java9
    Benchmark                                                  Mode  Cnt   Score   Error  Units
    VarHandleGeneratorBenchmark.getAndSetAndGetGeneration      avgt    5   2.558 ± 0.055  ns/op
    VarHandleGeneratorBenchmark.getGeneration                  avgt    5   2.188 ± 0.046  ns/op
    VarHandleGeneratorBenchmark.racyGetAndSetAndGetGeneration  avgt    5  20.043 ± 0.359  ns/op
    VarHandleMarkHolderBenchmark.link                          avgt    5   3.792 ± 0.048  ns/op
    VarHandleMarkHolderBenchmark.start_marker_noTag            avgt    5   4.225 ± 0.021  ns/op
    VarHandleMarkHolderBenchmark.start_marker_tag              avgt    5   4.983 ± 0.090  ns/op
    VarHandleMarkHolderBenchmark.start_name_noTag              avgt    5   3.777 ± 0.052  ns/op
    VarHandleMarkHolderBenchmark.start_name_tag                avgt    5   4.924 ± 0.106  ns/op
    VarHandleMarkHolderBenchmark.stop_marker_noTag             avgt    5   4.267 ± 0.072  ns/op
    VarHandleMarkHolderBenchmark.stop_marker_tag               avgt    5   5.857 ± 0.018  ns/op
    VarHandleMarkHolderBenchmark.stop_name_noTag               avgt    5   3.832 ± 0.062  ns/op
    VarHandleMarkHolderBenchmark.stop_name_tag                 avgt    5   5.149 ± 0.095  ns/op

---
+++ java6/src/jmh/java/io/perfmark/java6/SynchronizedMarkHolderBenchmark.java
@@ -0,0 +1,81 @@
+package io.perfmark.java6;
+
+import io.perfmark.impl.Marker;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class SynchronizedMarkHolderBenchmark {
+
+  public final Marker MARKER = Marker.NONE;
+
+  public final SynchronizedMarkHolder markHolder = new SynchronizedMarkHolder(16384);
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  public void start_name_tag() {
+    markHolder.start(1, "hi", "tag", 2, 1234);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  public void start_name_noTag() {
+    markHolder.start(1, "hi", 1234);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  public void start_marker_tag() {
+    markHolder.start(1, MARKER, "tag", 2, 1234);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  public void start_marker_noTag() {
+    markHolder.start(1, MARKER, 1234);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  public void stop_name_tag() {
+    markHolder.stop(1, "hi", "tag", 2, 1234);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  public void stop_name_noTag() {
+    markHolder.stop(1, "hi", 1234);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  public void stop_marker_tag() {
+    markHolder.stop(1, MARKER, "tag", 2, 1234);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  public void stop_marker_noTag() {
+    markHolder.stop(1, MARKER, 1234);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  public void link() {
+    markHolder.link(1, 9999, MARKER);
+  }
+}
