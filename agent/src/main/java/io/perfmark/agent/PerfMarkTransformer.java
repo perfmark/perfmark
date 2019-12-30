a36fafb220b73a40fd0bf764e6fc600a7a05fe35
Tomo Suzuki <suztomo@google.com>
2019-12-30 13:01:15 -0500

    PerfMarkMethodRewriter.PerfMarkMethodVisitor

--- agent/src/main/java/io/perfmark/agent/PerfMarkTransformer.java
+++ agent/src/main/java/io/perfmark/agent/PerfMarkTransformer.java
@@ -257,7 +257,7 @@ final class PerfMarkTransformer implements ClassFileTransformer {
     @Override
     public MethodVisitor visitMethod(
         int access, String name, String descriptor, String signature, String[] exceptions) {
-      return new PerfMarkTransformer.PerfMarkMethodRewriter.PerfMarkMethodVisitor(
+      return new PerfMarkMethodRewriter.PerfMarkMethodVisitor(
           perfmarkClassReader.api(),
           super.visitMethod(access, name, descriptor, signature, exceptions),
           name);
