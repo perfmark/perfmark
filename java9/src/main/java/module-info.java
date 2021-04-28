6a9145717e836d2e1e199488639521a7237603ec
Carl Mastrangelo <carl-mastrangelo@users.noreply.github.com>
2021-04-27 19:30:55 -0700

    all: Add automatic module support
    This makes Perfmark compatible with thew Java 9 Module system. For classes compiled for 8 and lower, an Automatic-Module-Name field is added to the MANIFEST.MF, to ensure a stable module name. (and not derive it from the jar name). For the java9 package, an actual module descriptor is added, which I have tested locally works.

--- java9/src/main/java/module-info.java
+++ java9/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,9 @@
  * limitations under the License.
  */
 
-/** Java 9+ enabled generator and mark-holder. */
-@javax.annotation.CheckReturnValue
-@javax.annotation.ParametersAreNonnullByDefault
-package io.perfmark.java9;
+module io.perfmark.java9 {
+  requires io.perfmark.impl;
+
+  opens io.perfmark.java9 to
+      io.perfmark.impl;
+}
