104ab113593893f3ba1cd1406a9b3644252443c6
Carl Mastrangelo <carl-mastrangelo@users.noreply.github.com>
2021-05-17 00:04:54 -0700

    impl,agent: Remove Markers and replace it with String calls
    While less precise, the string version of callsites is much easier to work with. It avoids needing to mess with clinit in classes, avoids needing to check that Marker is on the classpath, and avoids a 2 pass scan of the class file. Marker is nice, but adding the call site via a tag is much simpler to implement.
    
    This also removes the Java formatter plugin, as it doesn't yet work JDK16, and appears to be unmaintained.

---
+++ agent/src/test/java/io/perfmark/agent/TransformerTestClasses.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.perfmark.agent;
+
+import io.perfmark.Link;
+import io.perfmark.PerfMark;
+import io.perfmark.Tag;
+import java.util.concurrent.Executor;
+import java.util.function.Consumer;
+
+final class TransformerTestClasses {
+
+  static final class ClzAutoRecord {
+    public ClzAutoRecord() {
+      recordMe();
+    }
+
+    void recordMe() {
+      // seemingly nothing here.
+    }
+  }
+
+  record SomeRecord(int hi) {
+    public SomeRecord {
+      PerfMark.startTask("task");
+      PerfMark.stopTask("task");
+    }
+  }
+
+  static final class ClzCtorLambda implements Executor {
+    public ClzCtorLambda() {
+      execute(
+          () -> {
+            PerfMark.startTask("task");
+            PerfMark.stopTask("task");
+          });
+    }
+
+    @Override
+    public void execute(Runnable command) {
+      command.run();
+    }
+  }
+
+  static final class ClzWithClinit {
+    static {
+      Tag tag = PerfMark.createTag("tag", 1);
+      PerfMark.startTask("task");
+      PerfMark.stopTask("task");
+      PerfMark.startTask("task", tag);
+      PerfMark.stopTask("task", tag);
+    }
+  }
+
+  static final class ClzWithInit {
+    {
+      Tag tag = PerfMark.createTag("tag", 1);
+      PerfMark.startTask("task");
+      PerfMark.stopTask("task");
+      PerfMark.startTask("task", tag);
+      PerfMark.stopTask("task", tag);
+    }
+  }
+
+  static final class ClzWithCtor {
+    public ClzWithCtor() {
+      Tag tag = PerfMark.createTag("tag", 1);
+      PerfMark.startTask("task");
+      PerfMark.stopTask("task");
+      PerfMark.startTask("task", tag);
+      PerfMark.stopTask("task", tag);
+    }
+  }
+
+  static final class ClzWithLinks {
+    public ClzWithLinks() {
+      PerfMark.startTask("task");
+      Link link = PerfMark.linkOut();
+      PerfMark.linkIn(link);
+      PerfMark.stopTask("task");
+    }
+  }
+
+  public interface InterfaceWithDefaults {
+    default void record() {
+      Tag tag = PerfMark.createTag("tag", 1);
+      PerfMark.startTask("task");
+      PerfMark.stopTask("task");
+      PerfMark.startTask("task", tag);
+      PerfMark.stopTask("task", tag);
+    }
+  }
+
+  static final class Bar implements InterfaceWithDefaults {
+    public Bar() {
+      record();
+    }
+  }
+
+  static final class ClzWithMethodRefs {
+    public ClzWithMethodRefs() {
+      execute(PerfMark::startTask);
+      execute(PerfMark::stopTask);
+    }
+
+    void execute(Consumer<String> method) {
+      method.accept("task");
+    }
+  }
+
+  private TransformerTestClasses() {}
+}
+
+final class ClzFooter {
+  {
+    Tag tag = PerfMark.createTag("tag", 1);
+    PerfMark.startTask("task");
+    PerfMark.stopTask("task");
+    PerfMark.startTask("task", tag);
+    PerfMark.stopTask("task", tag);
+  }
+}
