6e1581f09be15e73824bda5eb70f1b5d78a6e6fd
Carl Mastrangelo <carl@carlmastrangelo.com>
2021-05-22 14:57:14 -0700

    try out kotlin

---
+++ traceviewer/build.gradle.kts
@@ -0,0 +1,31 @@
+buildscript {
+    extra.apply{
+        set("moduleName", "io.perfmark.traceviewer")
+    }
+    project.extra.set("libraries", extra.get("libraries"))
+}
+
+description = "PerfMark Trace Viewer"
+
+val jdkVersion = JavaVersion.VERSION_1_8
+
+tasks.getByName<JavaCompile>("compileJava") {
+    sourceCompatibility = jdkVersion.toString()
+    targetCompatibility = jdkVersion.toString()
+
+    options.compilerArgs.add("-Xlint:-options")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Automatic-Module-Name"] = "io.perfmark.traceviewer"
+    }
+}
+
+dependencies {
+    val libraries = project.extra.get("libraries") as Map<String, String>
+    compileOnly(libraries["jsr305"]!!)
+    compileOnly(libraries["errorprone"]!!)
+    implementation(project(":perfmark-tracewriter"))
+    testImplementation(project(":perfmark-api"))
+}
