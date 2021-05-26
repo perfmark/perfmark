1e44cf1aaccff12b99a070c79dc22c4d399105b0
Carl Mastrangelo <carl@carlmastrangelo.com>
2021-05-26 02:54:11 -0700

    api: add security manager test

--- api/src/test/java/io/perfmark/PerfMarkTest.java
+++ api/src/test/java/io/perfmark/PerfMarkTest.java
@@ -33,13 +33,14 @@ import java.security.Permission;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+import java.util.PropertyPermission;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Filter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import org.junit.Ignore;
+import java.util.logging.LoggingPermission;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -70,7 +71,6 @@ public class PerfMarkTest {
     logger.setLevel(Level.ALL);
     logger.setFilter(filter);
     try {
-      System.setProperty("io.perfmark.debug", "true");
       runWithProperty(System.getProperties(), "io.perfmark.PerfMark.debug", "true", () -> {
         try {
           // Force Initialization.
@@ -92,46 +92,161 @@ public class PerfMarkTest {
     Truth.assertThat(ref.get().getMessage()).contains("Error during PerfMark.<clinit>");
   }
 
-  @Test
-  @Ignore // Disabled for now
-  public void worksWithSecurityManager() throws Exception {
-    final class NopeSecurityManager extends SecurityManager {
-      boolean unload;
-      @Override
-      public void checkPermission(Permission perm) {
-        System.out.println(perm);
-        if (unload && perm.getName().equals("setSecurityManager")) {
-          return;
-        }
-        if (perm instanceof FilePermission) {
-          FilePermission fp = (FilePermission) perm;
-          if ("read".equals(fp.getActions())) {
-            if (fp.getName().endsWith(".class") && fp.getName().contains("io/perfmark/")) {
-              return;
-            }
+  private static class HesitantSecurityManager extends SecurityManager {
+    boolean unload;
+
+    @Override
+    public void checkPermission(Permission perm) {
+      if (unload && perm.getName().equals("setSecurityManager")) {
+        return;
+      }
+      if (perm instanceof FilePermission) {
+        FilePermission fp = (FilePermission) perm;
+        if ("read".equals(fp.getActions())) {
+          if (fp.getName().endsWith(".class") && fp.getName().contains("io/perfmark/")) {
+            return;
           }
         }
-        super.checkPermission(perm);
-        //throw new SecurityException("nope");
+      }
+      if (perm instanceof PropertyPermission) {
+        if (perm.getName().equals("java.util.logging.manager") && perm.getActions().equals("read")) {
+          return;
+        }
+      }
+      for (StackTraceElement element : new Throwable().getStackTrace()) {
+        if (element.getClassName().equals(TestClassLoader.class.getName())) {
+          if (perm.getName().equals("suppressAccessChecks")) {
+            return;
+          }
+          if (perm.getName().equals("accessSystemModules")) {
+            return;
+          }
+        }
+        if (element.getClassName().equals("java.util.logging.Level")) {
+          if (perm.getName().equals("suppressAccessChecks")) {
+            return;
+          }
+          if (perm.getName().equals("accessSystemModules")) {
+            return;
+          }
+        }
+        if (element.getClassName().equals("java.util.logging.LogManager")) {
+          if (perm.getName().equals("shutdownHooks")) {
+            return;
+          }
+          if (perm.getName().equals("setContextClassLoader")) {
+            return;
+          }
+          if (perm instanceof LoggingPermission && perm.getName().equals("control")) {
+            return;
+          }
+        }
+        if (element.getClassName().equals("java.util.logging.Logger")) {
+          if (perm.getName().equals("sun.util.logging.disableCallerCheck")) {
+            return;
+          }
+          if (perm.getName().equals("getClassLoader")) {
+            return;
+          }
+        }
       }
 
-      @Override
-      public void checkPermission(Permission perm, Object context) {
-        //throw new SecurityException("nope");
-      }
+      super.checkPermission(perm);
     }
+  }
+
+  @Test
+  public void worksWithSecurityManager_noStartEnabled_noDebug() throws Exception {
     ClassLoader loader = new TestClassLoader(getClass().getClassLoader());
 
     SecurityManager oldMgr = System.getSecurityManager();
-    NopeSecurityManager newMgr = new NopeSecurityManager();
+    HesitantSecurityManager newMgr = new HesitantSecurityManager();
+    Class<?> clz;
     try {
       System.setSecurityManager(newMgr);
-      Class.forName(PerfMark.class.getName(), true, loader);
+      clz = Class.forName(PerfMark.class.getName(), true, loader);
+      clz.getMethod("setEnabled", boolean.class).invoke(null, true);
+      clz.getMethod("event", String.class).invoke(null, "event");
     } finally {
       newMgr.unload = true;
       System.setSecurityManager(oldMgr);
     }
+
+    Class<?> storageClass = Class.forName(Storage.class.getName(), true, clz.getClassLoader());
+    List<Mark> marks = (List<Mark>) storageClass.getMethod("readForTest").invoke(null);
+    Truth.assertThat(marks).hasSize(1);
   }
+
+  @Test
+  public void worksWithSecurityManager_startEnabled_noDebug() throws Exception {
+    ClassLoader loader = new TestClassLoader(getClass().getClassLoader());
+
+    SecurityManager oldMgr = System.getSecurityManager();
+    HesitantSecurityManager newMgr = new HesitantSecurityManager() {
+      @Override
+      public void checkPermission(Permission perm) {
+        if (perm instanceof PropertyPermission) {
+          if (perm.getName().equals("io.perfmark.PerfMark.startEnabled") && perm.getActions().equals("read")) {
+            return;
+          }
+        }
+        super.checkPermission(perm);
+      }
+    };
+
+    Class<?> clz = runWithProperty(System.getProperties(), "io.perfmark.PerfMark.startEnabled", "true", () -> {
+      try {
+        System.setSecurityManager(newMgr);
+        Class<?> clz2 = Class.forName(PerfMark.class.getName(), true, loader);
+        clz2.getMethod("event", String.class).invoke(null, "event");
+        return clz2;
+      } finally {
+        newMgr.unload = true;
+        System.setSecurityManager(oldMgr);
+      }
+    });
+
+    Class<?> storageClass = Class.forName(Storage.class.getName(), true, clz.getClassLoader());
+    List<Mark> marks = (List<Mark>) storageClass.getMethod("readForTest").invoke(null);
+    Truth.assertThat(marks).hasSize(1);
+  }
+
+  @Test
+  public void worksWithSecurityManager_startEnabled_debug() throws Exception {
+    ClassLoader loader = new TestClassLoader(getClass().getClassLoader());
+
+    SecurityManager oldMgr = System.getSecurityManager();
+    HesitantSecurityManager newMgr = new HesitantSecurityManager() {
+      @Override
+      public void checkPermission(Permission perm) {
+        if (perm instanceof PropertyPermission) {
+          if (perm.getName().equals("io.perfmark.PerfMark.debug") && perm.getActions().equals("read")) {
+            return;
+          }
+        }
+        super.checkPermission(perm);
+      }
+    };
+
+    // TODO check logging occurred.
+
+    Class<?> clz = runWithProperty(System.getProperties(), "io.perfmark.PerfMark.debug", "true", () -> {
+      try {
+        System.setSecurityManager(newMgr);
+        Class<?> clz2 = Class.forName(PerfMark.class.getName(), true, loader);
+        clz2.getMethod("event", String.class).invoke(null, "event");
+        return clz2;
+      } finally {
+        newMgr.unload = true;
+        System.setSecurityManager(oldMgr);
+      }
+    });
+
+    Class<?> storageClass = Class.forName(Storage.class.getName(), true, clz.getClassLoader());
+    List<Mark> marks = (List<Mark>) storageClass.getMethod("readForTest").invoke(null);
+    Truth.assertThat(marks).hasSize(1);
+  }
+
 
 
   @Test
