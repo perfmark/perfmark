6688f9fd8f8ef3f7e2c2a9da9385d3c2d856e84c
Carl Mastrangelo <carl@carlmastrangelo.com>
2021-05-27 23:49:21 -0700

    agent: shade agent

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -1,7 +1,14 @@
+import groovy.util.Node
+
 buildscript {
     extra.apply {
         set("moduleName", "io.perfmark.agent")
     }
+}
+
+
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 val jdkVersion = JavaVersion.VERSION_1_6
@@ -38,9 +45,67 @@ tasks.named<JavaCompile>("compileTestJava") {
 }
 
 tasks.named<Jar>("jar") {
+    archiveClassifier.value("original")
     manifest {
         attributes(mapOf(
                 "Premain-Class" to "io.perfmark.agent.Main",
         ))
+    }
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    // make sure this is THE jar, which removes the suffix.
+    archiveClassifier.value(null as String?)
+
+    relocate("org.objectweb.asm", "io.perfmark.agent.shaded.org.objectweb.asm")
+}
+
+val javaComponent = components["java"] as AdhocComponentWithVariants
+javaComponent.withVariantsFromConfiguration(configurations["sourcesElements"]) {
+    skip()
+}
+
+publishing {
+    publications {
+        named<MavenPublication>("maven") {
+            artifacts.removeIf {
+                it.classifier.toString().contains("original")
+            }
+            artifact(tasks["shadowJar"])
+            artifact(tasks["sourcesJar"])
+
+            pom.withXml {
+                val root = asNode()
+
+                for (child in root.children()) {
+                    val c = child as Node
+                    if (c.name().toString().endsWith("dependencies")) {
+                        root.remove(c)
+                        break
+                    }
+                }
+            }
+        }
+
+        /*
+        maven(MavenPublication) {
+            // Ideally swap to project.shadow.component(it) when it isn't broken for project deps
+            artifact shadowJar
+                    // Empty jars are not published via withJavadocJar() and withSourcesJar()
+                    artifact javadocJar
+                    artifact sourcesJar
+
+                    pom.withXml {
+                        def dependencies = asNode().appendNode('dependencies')
+                        project.configurations.shadow.allDependencies.each { dep ->
+                            def dependencyNode = dependencies.appendNode('dependency')
+                            dependencyNode.appendNode('groupId', dep.group)
+                            dependencyNode.appendNode('artifactId', dep.name)
+                            def version = (dep.name == 'grpc-core') ? '[' + dep.version + ']' : dep.version
+                            dependencyNode.appendNode('version', version)
+                            dependencyNode.appendNode('scope', 'compile')
+                        }
+                    }
+        }*/
     }
 }
