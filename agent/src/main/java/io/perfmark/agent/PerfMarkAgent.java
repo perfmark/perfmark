d48f25cc5115ee8f5f1694240cdf7029b881331b
Carl Mastrangelo <carl-mastrangelo@users.noreply.github.com>
2021-06-02 22:49:11 -0700

    agent: inject PerfMark into boot class loader for use in JDK classes.

--- agent/src/main/java/io/perfmark/agent/PerfMarkAgent.java
+++ agent/src/main/java/io/perfmark/agent/PerfMarkAgent.java
@@ -24,7 +24,7 @@ public final class PerfMarkAgent {
    * Entry point as an agent.
    */
   public static void premain(String agentArgs, Instrumentation inst) {
-    inst.addTransformer(new PerfMarkTransformer());
+    inst.addTransformer(new PerfMarkTransformer(inst));
   }
 
   private PerfMarkAgent() {}
