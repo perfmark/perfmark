9be3e6637df1a19459e22334a294fe004e587e7e
Carl Mastrangelo <carl@carlmastrangelo.com>
2023-02-03 18:38:41 -0800

    api: move api build over to kotlinscript

---
+++ api/build.gradle.kts
@@ -0,0 +1,96 @@
+@Suppress("DSL_SCOPE_VIOLATION") // See https://github.com/gradle/gradle/issues/22797
+plugins {
+    alias(libs.plugins.jmh)
+}
+
+buildscript {
+    extra.apply{
+        set("moduleName", "io.perfmark")
+    }
+}
+
+
+val jdkVersion = JavaVersion.VERSION_1_6
+
+description = "PerfMark API"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    sourceCompatibility = jdkVersion.toString()
+    targetCompatibility = jdkVersion.toString()
+
+    options.compilerArgs.add("-Xlint:-options")
+}
+
+dependencies {
+    compileOnly(libs.jsr305)
+    compileOnly(libs.errorprone)
+
+    testImplementation(project(":perfmark-impl"))
+    testImplementation(libs.truth)
+    testRuntimeOnly(project(":perfmark-java6"))
+
+    jmhImplementation(project(":perfmark-java9"))
+    jmhImplementation(project(":perfmark-java7"))
+}
+
+tasks.named<JavaCompile>("compileTestJava") {
+    sourceCompatibility = JavaVersion.VERSION_11.toString()
+    targetCompatibility = JavaVersion.VERSION_11.toString()
+}
+
+
+tasks.named<JavaCompile>("compileJmhJava") {
+    sourceCompatibility = JavaVersion.VERSION_11.toString()
+    targetCompatibility = JavaVersion.VERSION_11.toString()
+}
+
+java {
+    disableAutoTargetJvm()
+}
+
+tasks.named<Javadoc>("javadoc") {
+    exclude("io/perfmark/Impl**")
+}
+
+
+jmh {
+
+    timeOnIteration.set("1s")
+    warmup.set("1s")
+    fork.set(400)
+    warmupIterations.set(0)
+
+    includes.add("ClassInit")
+    profilers.add("cl")
+    jvmArgs.add("-Dio.perfmark.debug=true")
+
+    /*
+    profilers = ["perfasm"]
+
+    jvmArgs = [
+            "-XX:+UnlockDiagnosticVMOptions",
+            "-XX:+LogCompilation",
+            "-XX:LogFile=/tmp/blah.txt",
+            "-XX:+PrintAssembly",
+            "-XX:+PrintInterpreter",
+            "-XX:+PrintNMethods",
+            "-XX:+PrintNativeNMethods",
+            "-XX:+PrintSignatureHandlers",
+            "-XX:+PrintAdapterHandlers",
+            "-XX:+PrintStubCode",
+            "-XX:+PrintCompilation",
+            "-XX:+PrintInlining",
+            "-XX:+TraceClassLoading",
+            "-XX:PrintAssemblyOptions=syntax",
+            "-XX:PrintAssemblyOptions=intel"
+    ]
+     */
+
+    //duplicateClassesStrategy DuplicatesStrategy.INCLUDE
+}
